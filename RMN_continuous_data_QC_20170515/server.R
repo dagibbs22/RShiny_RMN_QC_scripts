bd478c792741f269ddb31ad7f85d5b630e1aa529
Gibbs <gibbs.david@epa.gov>
2017-05-24 15:11:42 -0400

    Simplified input to just file selection box, operation selectoin dropdown, and output directory text box. User now manually inputs output directory name.

--- RMN_continuous_data_QC_20170515/server.R
+++ RMN_continuous_data_QC_20170515/server.R
@@ -1,34 +1,21 @@
-library(shiny)
-library(shinyFiles)
-library(devtools)
-source("testfunction_20170517.R")
-#install_github("leppott/ContDataQC")
-library("ContDataQC")
+source("global.R")
 
 #setwd("C:/Users/dgibbs/Documents/Projects/Regional_Monitoring_Networks/Continuous_data_processing/RShiny RMN QC scripts/RMN_continuous_data_QC_20170515")
 
 
 shinyServer(function(input, output, session) {
 
-  #The name of the field which has the dates
-  dateField <- reactive({
-    dateList <- input$dateField
-  })
-  
   #Establishes the input spreadsheet. datasetInput is referred to in later sections.
   datasetInput <- reactive({
-    infile <- input$file1
-    if (is.null(infile))
+    inFile <- input$file1
+    if (is.null(inFile))
       return(NULL)
-    read.csv(infile$datapath, header=TRUE)
+    read.csv(inFile$datapath, header=TRUE)
   })
   
   #Operation to be performed on the data (e.g., QC, aggregation)
   output$Operation <- renderText({paste("input$Operation is '",input$Operation,"'.",sep="")})
-  
-  #Type of data being input (e.g., air, water, air/water)
-  output$DataType <- renderText({paste("input$DataType is '",input$DataType,"'.",sep="")})
-  
+
   #Not currently in use
   output$BaseDir <- renderText(({paste("input$BaseDir is '",input$BaseDir,"'.",sep="")}))
   
@@ -36,8 +23,8 @@ shinyServer(function(input, output, session) {
   output$dates <- renderUI({
     
     #Makes it so the sidebar doesn't show an error just because dates haven't been selected
-    infile <- input$file1
-    if (is.null(infile))
+    inFile <- input$file1
+    if (is.null(inFile))
       return(NULL)
     
     #creates a copy of the input spreadsheet. Referred to later.
@@ -66,53 +53,69 @@ shinyServer(function(input, output, session) {
   #so users can check whether the right files are selected.
   #Each input spreadsheet gets one row.
   output$summaryTable <- renderTable({
-    
+
     #Makes it so the table area doesn't show an error just because dates haven't been selected
     inFile <- input$file1
     if (is.null(inFile))
       return(NULL)
-    
+
     #Creates a copy of the input spreadsheet. Referred to later.
     measurements <- datasetInput()
-    
-    #Identifies the column number which has the dates 
-    #based on the user's input of the name of the date field
-    inDates <- input$dateField
-    dateColNumb <- which(colnames(measurements)==inDates)
-    
+
     #Extracts the name of the file from the input file
     filename <- inFile$name
-    
-    #Extracts the station ID from the input file
-    stationID <- measurements[1,2]
 
-    #Calculates the starting and ending dates for each file
-    #The date field is based on the user-input date field name
-    dates <- as.Date(measurements[,dateColNumb], format = "%m/%d/%Y")
-    minval <- which.min(as.Date(measurements[,dateColNumb], format = "%m/%d/%Y"))
-    maxval <- which.max(as.Date(measurements[,dateColNumb], format = "%m/%d/%Y"))
-    minDate <- measurements[minval,dateColNumb]
-    maxDate <- measurements[maxval,dateColNumb]
+    #Runs a function to extract the station ID, data type, and start and
+    #end dates from the input file name.
+    #The returned object is a data.frame, in which each column has one
+    #of the attributes.
+    fileAttribs <- nameParse(filename)
+
+    #Creates objects for the station ID, type of data in the file
+    #(e.g., Air, Air & Water, Water) and start and end dates of
+    #the file
+    stationID <- fileAttribs[1,1]
+    dataType <- fileAttribs[1,2]
+    startDate <- fileAttribs[1,3]
+    endDate <- fileAttribs[1,4]
 
     #Extracts how many records are in the spreadsheet
     recordCount <- nrow(measurements)
-    
+
     #Creates the summary table with column headings
-    summaryTable <- data.frame(filename, stationID, minDate, maxDate, recordCount)
-    columns <- c("File name", "Station ID", "Starting date", "Ending date", "Record count")
+    summaryTable <- data.frame(filename, stationID, dataType, startDate, endDate, recordCount)
+    columns <- c("File name", "Station ID", "Data type", "Starting date", "Ending date", "Record count")
     colnames(summaryTable) <- columns
+
+    #Reformats the date columns to be the right date format
+    summaryTable[,4] <- format(summaryTable[,4], "%Y-%m-%d")
+    summaryTable[,5] <- format(summaryTable[,5], "%Y-%m-%d")
+
     return(summaryTable)
-    
+
   })
   
   #Purely for testing purposes. To make sure text is being interpreted properly.
   output$file <- renderText({
-    inDates <- input$dateField
-    measurements <- datasetInput()
     
-    dateColNumb <- which(colnames(measurements)==inDates)
-    #paste("This is for testing", inDates, dateColNumb)
-    paste("This is for more testing", input$expDateRange[1], input$expDateRange[2])
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+
+    #Creates a copy of the input spreadsheet. Referred to later.
+    measurements <- datasetInput()
+
+    #Extracts the name of the file from the input file
+    filename <- inFile$name
+
+    fileAttribs <- nameParse(filename)
+
+    stationID <- fileAttribs[1,1]
+    dataType <- fileAttribs[1,2]
+    minval <- fileAttribs[1,3]
+    maxval <- fileAttribs[1,4]
+
+    paste("This is for more testing", input$outputDir)
   })
   
   #Prints the first four lines of the input spreadsheet. For testing purposes.
@@ -128,30 +131,44 @@ shinyServer(function(input, output, session) {
     head(read.csv(inFile$datapath),4)
   })
   
-  #Runs the selected process by calling on the QC script that Erik Leppo wrote
+    #Runs the selected process by calling on the QC script that Erik Leppo wrote
   observeEvent(input$runProcess, {
-    
-    #Creates a copy of the input spreadsheet. Referred to later.
-    measurements <- datasetInput()
-    
-    #Identifies the field that has the station ID based on the user input
-    inStationIDs <- input$stationIDField
-    stationIDColNumb <- which(colnames(measurements)==inStationIDs)
-    stationIDs <- measurements[1, stationIDColNumb]
-    
-    #Pulls in the starting and ending dates, which the user can modify but
-    #are originally based on the first and last dates in the file
-    firstDate <- as.character(input$expDateRange[1], format = "%Y/%m/%d")
-    lastDate <- as.character(input$expDateRange[2], format = "%Y/%m/%d")
 
+    inFile <- input$file1
+
+    #Extracts the name of the file from the input file
+    filename <- inFile$name
+    
+    #Runs a function to extract the station ID, data type, and start and
+    #end dates from the input file name.
+    #The returned object is a data.frame, in which each column has one
+    #of the attributes.
+    fileAttribs <- nameParse(filename)
+    
+    #Creates objects for the station ID, type of data in the file
+    #(e.g., Air, Air & Water, Water) and start and end dates of
+    #the file
+    stationID <- fileAttribs[1,1]
+    dataType <- fileAttribs[1,2]
+    startDate <- fileAttribs[1,3]
+    endDate <- fileAttribs[1,4]
+    
+    stationID <- as.character(stationID)
+    dataType <- as.character(dataType)
+    startDate <- format(startDate, "%Y-%m-%d")
+    endDate <- format(endDate, "%Y-%m-%d")
+    
+    #Renames the output folder object
+    outputFolder <- input$outputDir
+    
     #Invokes the QC/aggregate/summarize script
     ContDataQC(input$Operation, 
-               stationIDs, 
-               input$DataType, 
-               firstDate,
-               lastDate,
-               "C:/Users/dgibbs/Documents/Projects/Regional_Monitoring_Networks/Continuous_data_processing/RShiny RMN QC scripts/RMN_continuous_data_QC_20170515", 
-               "", 
+               stationID, 
+               dataType, 
+               startDate,
+               endDate,
+               "C:/Users/dgibbs/Documents/Projects/Regional_Monitoring_Networks/Continuous_data_processing/RShiny RMN QC scripts/RMN_continuous_data_QC_20170515/Data1_RAW", 
+               outputFolder,
                "")
   })
   
--- RMN_continuous_data_QC_20170515/ui.R
+++ RMN_continuous_data_QC_20170515/ui.R
@@ -14,20 +14,8 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
 
-      #Input for the name of the field in the input spreadsheets containing the date
-       textInput("dateField",
-                 label="Field with date-time values (must not have spaces)",
-                 value = "",
-                 placeholder = NULL)
-      
-       #Input for the name of the field in the input spreadsheets containing the station ID
-      ,textInput("stationIDField",
-                 label="Field with station ID values",
-                 value = "",
-                 placeholder = NULL)
-      
       #The selected input file
-      ,fileInput("file1",label="Choose File", multiple = TRUE)
+      fileInput("file1",label="Choose File", multiple = TRUE)
       
       #Operation to be performed on the selected data
       ,selectInput("Operation", 
@@ -35,54 +23,20 @@ shinyUI(fluidPage(
                   choices = c("GetGageData", "QCRaw", "Aggregate", "SummaryStats"),
                   selected = "QCRaw")
 
-      #Type of data to be operated on
-      ,selectInput("DataType", 
-                    label = "Choose a type of data file",
-                    choices = c("Air","Water","AW","Gage","AWG","AG","WG" ),
-                    selected = "AW")
-
-      #The starting and ending dates for the process.
-      #Initially populated by the first and last dates in the spreadsheet
-      #but can be modified by the user
-      ,uiOutput("dates")
-      
-      #Not currently using
-      # ,dateRangeInput('dateRange',
-      #                 label = 'Date range input: yyyy-mm-dd',
-      #                 start = Sys.Date() - 2, end = Sys.Date() + 2
-      # )
-      
-      #       selectInput("DirImport", 
-      #                   label = "Choose import directory for data (can be set by operation)",
-      #                   choices = c("Data1_RAW","Data2_QC","Data3_Aggregated","Data4_Stats" ),
-      #                   selected = "Olsen Model Background"),
-      #       
-      #       selectInput("DirExport", 
-      #                   label = "Choose export directory for data (can be set by operation)",
-      #                   choices = c("Data1_RAW","Data2_QC","Data3_Aggregated","Data4_Stats" ),
-      #                   selected = "Olsen Model Background"),
-      
       #Not currently using
       #,helpText("HelpText",label="Help Text")
       
-      #Not currently using
-      ,textInput("BaseDir", label="Base directory for data.", value = "", width = NULL, placeholder = NULL)
-      
-      #Runs the process
-      ,actionButton('runProcess',label='Run Operation')
+      #User types or copies in the full output directory
+      ,textInput("outputDir", label="Output directory for data", value = "", width = NULL, placeholder = NULL)
+
+      #Runs the selected process
+      ,actionButton("runProcess",label='Run Operation')
       
     ),##sidebarpanel.END
     
 
     mainPanel(
-      # 
-      # h4("output$dir"),
-      # verbatimTextOutput("dir"), br(),
-      # h4("Files in that dir"),
-      # verbatimTextOutput("files")
-      
-      #DT::dataTableOutput("table"),
-      
+
       #Outputs the testing text, the table with properties of the input spreadsheets,
       #and a testing table of the beginning of the spreadsheets
       textOutput("file")
