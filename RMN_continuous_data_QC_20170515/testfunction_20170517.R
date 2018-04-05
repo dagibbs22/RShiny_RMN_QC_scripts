d16a1b5d8d2266276484e756779554778ef94c60
dagibbs22 <dagibbs22@gmail.com>
2018-04-05 15:48:19 -0400

    Delete testfunction_20170517.R

--- RMN_continuous_data_QC_20170515/testfunction_20170517.R
+++
@@ -1,6 +0,0 @@
-testcsv <- function(y,z) {
-  setwd("C:\\Users\\dgibbs\\Documents\\Projects\\Regional_Monitoring_Networks\\Continuous_data_processing\\RShiny RMN QC scripts\\RMN_continuous_data_QC_20170515\\Data2_QC")
-  x <- rpois (y, z)
-  testcsv <- write.csv(x, file = "testcsv.csv")
-  return (testcsv)
-}
